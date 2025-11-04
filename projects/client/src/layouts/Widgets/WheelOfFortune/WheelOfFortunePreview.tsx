9cc03264421c3f9529a86648fc0f5ce51f10005e
Iakov Tatarinov <36223182+venom7212@users.noreply.github.com>
2025-11-04 15:00:24 +0500

    fix: add text, preview CSS

--- projects/client/src/layouts/Widgets/WheelOfFortune/WheelOfFortunePreview.tsx
+++ projects/client/src/layouts/Widgets/WheelOfFortune/WheelOfFortunePreview.tsx
@@ -2,7 +2,7 @@ import useWidgetPreviewStore from '@/stores/widgetPreviewStore'
 import DesktopPreview from '../Common/DesktopPreview/DesktopPreview'
 import MobilePreview from '../Common/MobilePreview/MobilePreview'
 import useWidgetSettingsStore from '@/stores/widgetSettingsStore'
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 type WheelOfFortunePreviewProps = {
   spinTrigger?: number
@@ -16,12 +16,14 @@ const WheelOfFortunePreview = ({ spinTrigger }: WheelOfFortunePreviewProps) => {
 
   const ref = useRef<HTMLDivElement>(null)
   const modalWindowRef = useRef<HTMLDivElement>(null)
+  const [modalWidth, setModalWidth] = useState<number | null>(null)
 
   useEffect(() => {
     if (ref?.current && modalWindowRef?.current) {
       const scaleFactor = ref.current.clientWidth / modalWindowRef.current.clientWidth
       ref.current.style.transform = `scale(${scaleFactor})`
       ref.current.style.transformOrigin = 'top left'
+      setModalWidth(modalWindowRef.current.clientWidth)
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [ref?.current, modalWindowRef?.current])
@@ -30,14 +32,28 @@ const WheelOfFortunePreview = ({ spinTrigger }: WheelOfFortunePreviewProps) => {
 
   if (windowFormat === 'modalWindow') {
     return (
-      <div ref={ref} className="flex flex-col gap-10">
-        <DesktopPreview
-          ref={modalWindowRef}
-          screen="main"
-          onSubmit={() => {}}
-          spinTrigger={spinTrigger}
-        />
-        <DesktopPreview screen="prize" onSubmit={() => {}} spinTrigger={spinTrigger} />
+      <div ref={ref} className="flex flex-col gap-5">
+        <div>
+          <p className="font-rubik text-[25px] py-[15px]">Главный экран</p>
+          <DesktopPreview
+            ref={modalWindowRef}
+            screen="main"
+            onSubmit={() => {}}
+            spinTrigger={spinTrigger}
+          />
+        </div>
+        <hr
+          className="scale-100 h-px border-0 bg-default-300 self-start "
+          style={{ width: modalWidth ? `${modalWidth}px` : undefined }}
+        ></hr>
+        <div>
+          <p className="font-rubik text-[25px] mb-5">Призовой экран</p>
+          <DesktopPreview 
+            screen="prize" 
+            onSubmit={() => {}} 
+            ref={modalWindowRef}
+            spinTrigger={spinTrigger} />
+        </div>
       </div>
     )
   }
