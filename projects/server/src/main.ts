b7aa7feb28423eb8aa84196acacc2cdb858d082a
Albert <niksers@mail.ru>
2025-12-18 00:35:09 +0500

    LEM-57: fix formatting

--- projects/server/src/main.ts
+++ projects/server/src/main.ts
@@ -28,7 +28,10 @@ async function bootstrap() {
   app.use('/api/public', (req: Request, res: Response, next: NextFunction) => {
     res.header('Access-Control-Allow-Origin', '*')
     res.header('Access-Control-Allow-Methods', 'GET,OPTIONS')
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    )
     res.header('Access-Control-Allow-Credentials', 'false')
     if (req.method === 'OPTIONS') {
       res.sendStatus(204)
