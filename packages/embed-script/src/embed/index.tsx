5734f7409a96e8ccf5a88710e57f5963cdcbdd0a
Albert <niksers@mail.ru>
2025-12-22 06:36:06 +0500

    LEM-57: import CSS styles for embed script

--- packages/embed-script/src/embed/index.tsx
+++ packages/embed-script/src/embed/index.tsx
@@ -1,6 +1,7 @@
 import type { InitOptions } from './types'
 import EmbedManager from './embedManager'
 import { findEmbedScript } from './utils'
+import './embed.css'
 
 const manager = new EmbedManager()
 const currentScript = findEmbedScript()
