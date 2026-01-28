12cfbc5f7d43f07f4222e6d3628d6ca0590e9753
wthoutanymmries <6926634@gmail.com>
2026-01-28 11:14:09 +0500

    LEM-126: cleaned up comments

--- projects/client/src/layouts/Widgets/FABMenu/FABMenuField.tsx
+++ projects/client/src/layouts/Widgets/FABMenu/FABMenuField.tsx
@@ -4,15 +4,11 @@ import BorderedContainer from '@/layouts/BorderedContainer/BorderedContainer'
 import { useWidgetStaticDefaults } from '@/stores/widgetSettingsStore'
 import { WidgetTypeEnum } from '@lemnity/api-sdk'
 import { useFABMenuSettings } from './hooks'
-import type {
-  // FABMenuIconKey,
-  FABMenuSectorItem,
-  FABMenuWidgetSettings
-} from '@/layouts/Widgets/FABMenu/types'
+import type { FABMenuSectorItem, FABMenuWidgetSettings } from '@/layouts/Widgets/FABMenu/types'
 import FABSectorItem from './FABSectorItem'
 import FABMenuButtonPicker from './FABMenuButtonPicker'
 import { createPlaceholderFABMenuSector } from './defaults'
-import { /*FAB_MENU_BUTTON_PRESETS,*/ type FABMenuButtonDefinition } from './buttonLibrary'
+import { type FABMenuButtonDefinition } from './buttonLibrary'
 import DisableBranding from '@/layouts/WidgetSettings/FieldsSettingsTab/DisableBranding/DisableBranding'
 
 const FABMenuField = () => {
@@ -63,21 +59,6 @@ const FABMenuField = () => {
   }
 
   const handleLabelChange = (index: number, label: string) => updateFABMenuSector(index, { label })
-  // const handleIconChange = (index: number, icon: FABMenuIconKey) => {
-  //   const preset = FAB_MENU_BUTTON_PRESETS.find(entry => entry.icon === icon)
-  //   if (!preset) {
-  //     updateFABMenuSector(index, { icon })
-  //     return
-  //   }
-
-  //   updateFABMenuSector(index, {
-  //     icon: preset.icon,
-  //     label: preset.label,
-  //     payload: preset.payload,
-  //     color: preset.color,
-  //     description: preset.description
-  //   })
-  // }
   const handlePayloadTypeChange = (index: number, type: FABMenuSectorItem['payload']['type']) =>
     updateFABMenuSector(index, {
       payload: {
@@ -119,13 +100,11 @@ const FABMenuField = () => {
           canReorder
           classNames={{
             index: 'min-w-[40px]'
-            // delete: 'min-w-[40px]'
           }}
           renderItem={(sector, index) => (
             <FABSectorItem
               sector={sector}
               onLabelChange={label => handleLabelChange(index, label)}
-              // onIconChange={icon => handleIconChange(index, icon)}
               onPayloadTypeChange={type => handlePayloadTypeChange(index, type)}
               onPayloadValueChange={value => handlePayloadValueChange(index, value)}
               onColorChange={color => handleColorChange(index, color)}
