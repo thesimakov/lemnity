7aa057718fe1845cdbe5aa8eb1a28505f24fe789
wthoutanymmries <6926634@gmail.com>
2026-02-04 15:48:35 +0500

    LEM-159: CountdownScreen is finished

--- projects/client/src/components/FreePlanBrandingLink.tsx
+++ projects/client/src/components/FreePlanBrandingLink.tsx
@@ -1,9 +1,10 @@
-const FreePlanBrandingLink = () => {
+const FreePlanBrandingLink = ({ color = '#797979' }) => {
   return (
     <a
       href="https://lemnity.ru"
       target="_blank"
-      className="hover:underline text-[#797979] text-xs leading-3 self-center"
+      className="hover:underline text-xs leading-3 self-center"
+      style={{ color: color }}
     >
       Создано на Lemnity
     </a>
--- projects/client/src/layouts/Widgets/Announcement/CountdownAnnouncementWidget.tsx
+++ projects/client/src/layouts/Widgets/Announcement/CountdownAnnouncementWidget.tsx
@@ -6,30 +6,38 @@ import lemnityBlackLogo from '@/assets/logos/lemnity-black-logo.svg'
 import crossIcon from '@/assets/icons/cross.svg'
 import CountdownTimer from '@/components/CountdownTimer'
 import { useState } from 'react'
+import FreePlanBrandingLink from '@/components/FreePlanBrandingLink'
 
-const CoiuntdownScreen = () => {
+const CountdownScreen = () => {
+  // @ts-expect-error aboba
   const [initialTime, setInitialTime] = useState<number>(259200)
 
   return (
-    <div
-    className={cn(
-      'w-full flex flex-col items-center justify-center gap-3',
-      'mt-10.5 px-8.5',
-    )}
-    >
-      <span className="text-white font-bold text-[40px] leading-12 text-center">
-        До Нового года осталось
-      </span>
-      <span className="text-white text-[16px] leading-4.75 text-center">
-        Вы можете разместить здесь описание
-      </span>
+    <>
+      <div className="w-42 h-9.5 fill-white text-white">
+        <SvgIcon src={lemnityBlackLogo} alt="Company Logo" />
+      </div>
+    
+      <div
+      className={cn(
+        'w-full flex flex-col items-center justify-center gap-3.75',
+        'mt-3.75 px-8.5',
+      )}
+      >
+        <span className="text-white font-bold text-[40px] leading-12 text-center">
+          До Нового года осталось
+        </span>
+        <span className="text-white text-[16px] leading-4.75 text-center">
+          Вы можете разместить здесь описание
+        </span>
 
-      <CountdownTimer initialTime={initialTime} />
+        <CountdownTimer initialTime={initialTime} />
 
-      <Button className="w-full h-10.75 bg-[#FFB400] rounded-md">
-        Хочу скидку!
-      </Button>
-    </div>
+        <Button className="w-full h-10.75 bg-[#FFB400] rounded-md text-[20px]">
+          Хочу скидку!
+        </Button>
+      </div>
+    </>
   )
 }
 
@@ -37,25 +45,25 @@ const CountdownAnnouncementWidget = () => {
   return (
     <div
       className={cn(
-        'w-95 h-129.5 pt-14 rounded-2xl bg-[#725DFF]',
+        'w-99.5 h-129.5 pt-14 rounded-2xl bg-[#725DFF]',
         'flex flex-col items-center relative'
       )}
     >
-      <div className="w-42 h-9.5 fill-white text-white">
-        <SvgIcon src={lemnityBlackLogo} alt="Company Logo" />
-      </div>
-
       <Button
         className={cn(
-          'min-w-12 w-12 h-8.5 top-4.5 right-3.75 rounded-[5px] bg-white',
-          'absolute flex justify-center items-center'
+          'min-w-11.25 w-11.25 h-7.5 top-4.5 right-4.5 rounded-[5px] bg-white',
+          'px-0 absolute flex justify-center items-center'
         )}>
         <div className="w-4 h-4 fill-black">
           <SvgIcon src={crossIcon} alt="Close" />
         </div>
       </Button>
 
-      <CoiuntdownScreen />
+      <CountdownScreen />
+
+      <div className='mt-auto mb-4'>
+        <FreePlanBrandingLink color='#FFFFFF' />
+      </div>
     </div>
   )
 }
