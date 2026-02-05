1a202196328ba3cd21cad62a900121285742ebb4
wthoutanymmries <6926634@gmail.com>
2026-02-05 17:47:47 +0500

    LEM-159: background color and display settings are now adaptive components

--- projects/client/src/layouts/Widgets/Announcement/DisplaySettings/DisplaySettings.tsx
+++ projects/client/src/layouts/Widgets/Announcement/DisplaySettings/DisplaySettings.tsx
@@ -3,7 +3,7 @@ import WidgetType from "./WidgetType"
 
 const DisplaySettings = () => {
   return (
-    <div className="w-full flex flex-col gap-2.5">
+    <div className="w-full min-w-85.5 flex flex-col gap-2.5">
       <h1 className="text-[25px] leading-7.5 font-normal text-[#060606]">Оформление</h1>
       <WidgetType />
       <WidgetBackgroundColor />
